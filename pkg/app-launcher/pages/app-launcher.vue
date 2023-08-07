1df7eb0d333fc3b886b6fc3320375567345608a4
Cody Shearer <cjshearer@live.com>
2023-08-07 14:34:24 -0400

    fix: workaround incorrect ButtonDropdown z-index
    This commit adds `z-index: 1` to the cluster-header to ensure it stays
    above the ButtonDropdown in the `AppLancherCard` page.
    
    This shouldn't be necessary, since the position:sticky in the
    app-launcher page's header should make the header appear above its
    siblings, but the ButtonDropdown element seems to have the wrong z-index
    by default, so we just force it to be below the header. See the
    following slack conversation for previous discussion on this:
    https://krumware.slack.com/archives/C053W936JAZ/p1691418985551309?thread_ts=1691099848.592889&cid=C053W936JAZ

--- pkg/app-launcher/pages/app-launcher.vue
+++ pkg/app-launcher/pages/app-launcher.vue
@@ -52,7 +52,9 @@ export default {
       :key="cluster.id"
       style="margin-bottom: 2rem"
     >
-      <h1 class="cluster-header">
+      <h1
+        class="cluster-header hack-to-keep-header-above-app-launcher-card-dropdown-button"
+      >
         {{ cluster.name }}
       </h1>
       <div class="services-by-cluster-grid">
@@ -81,5 +83,15 @@ export default {
   height: var(--header-height);
   position: sticky;
   top: 0;
+}
+
+// HACK(cjshearer): this shouldn't be necessary, since the position:sticky in
+// the app-launcher page's header should make the header appear above its
+// siblings, but the AppLauncherCard's ButtonDropDown element seems to have the
+// wrong z-index by default, so we just force it to be below the header. See the
+// following slack conversation for previous discussion on this:
+// https://krumware.slack.com/archives/C053W936JAZ/p1691418985551309?thread_ts=1691099848.592889&cid=C053W936JAZ
+.hack-to-keep-header-above-app-launcher-card-dropdown-button {
+  z-index: 1;
 }
 </style>
