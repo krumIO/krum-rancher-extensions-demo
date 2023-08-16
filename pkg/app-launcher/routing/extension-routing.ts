90c0f9a1f7812c285a0ba478700abbaf118ce167
Colin Griffin <colin@krum.io>
2023-08-16 14:51:30 -0400

    remove dynamic import

--- pkg/app-launcher/routing/extension-routing.ts
+++ pkg/app-launcher/routing/extension-routing.ts
@@ -1,8 +1,9 @@
 import type { RouteConfig } from 'vue-router';
 import { PRODUCT_NAME } from '../config/app-launcher';
+import AppLauncher from '../pages/app-launcher';
 
 const MAIN_APP_LAUNCHER_LOCATION: RouteConfig = {
-  component: () => import('../pages/app-launcher.vue'),
+  component: AppLauncher,
   name: PRODUCT_NAME,
   path: `/${PRODUCT_NAME}`,
 };
