8201b5ba3c24db05e76f54001fb944bc39ab0842
Cody Shearer <cjshearer@live.com>
2023-08-21 15:53:38 -0400

    fix: revert 86ee67c5 (typescript/composition api)
    This commit reverts the changes made in
    86ee67c571a5f8e6214beac07adf79cab8acc6e9, which introduced usage the vue
    'composition' api and TypeScript, neither of which appears to be
    compatible with Rancher extensions. While perfectly functional in dev
    mode, a build run in production would fail due to rancher dashboard
    missing dependency updates.

--- pkg/app-launcher/components/AppLauncherCard.vue
+++ pkg/app-launcher/components/AppLauncherCard.vue
@@ -1,100 +1,102 @@
-<script lang="ts" setup>
+<script>
 import { Card } from '@components/Card';
 import ButtonDropDown from '@shell/components/ButtonDropdown';
 import { isMaybeSecure } from '@shell/utils/url';
-import { computed } from 'vue';
 
-const props = defineProps<{
-  clusterId: string;
-  service: AppLauncherService;
-}>();
-
-const endpoints = computed(
-  () =>
-    props.service.spec.ports?.map((port) => {
-      const endpoint = `${
-        isMaybeSecure(port.port, port.protocol) ? 'https' : 'http'
-      }:${props.service.metadata.name}:${port.port}`;
-
-      return {
-        label: `${endpoint}${port.protocol === 'UDP' ? ' (UDP)' : ''}`,
-        value: `k8s/clusters/${props.clusterId}/api/v1/namespaces/${props.service.metadata.namespace}/services/${endpoint}/proxy`,
-      };
-    }) ?? []
-);
-
-const computedServiceName = computed(() => {
-  if (props.service.metadata.labels?.['app.kubernetes.io/component']) {
-    return props.service.metadata.labels['app.kubernetes.io/component'];
-  }
-
-  return props.service.metadata.name;
-});
-
-const helmChart = computed(
-  () => props.service.metadata.labels?.['helm.sh/chart']
-);
-
-const kubernetesVersion = computed(
-  () => props.service.metadata.labels?.['app.kubernetes.io/version']
-);
-
-const name = computed(() => props.service.metadata.name);
-
-const namespace = computed(() => props.service.metadata.namespace);
-
-const openLink = (link: string) => {
-  window.open(link);
-};
-</script>
-
-<script lang="ts">
-// TODO(cjshearer): see if there are existing type definitions for k8s services
-// that we can use instead of manually providing the ones we need.
-export type AppLauncherService = {
-  id: string;
-  metadata: {
-    labels?: {
-      /**
-       * Helm chart name that created this app (if relevant).
-       */
-      'helm.sh/chart'?: string;
-      /**
-       * The component of the app.
-       *
-       * See `app.kubernetes.io/component` from:
-       * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-       */
-      'app.kubernetes.io/component'?: string;
-      /**
-       * The version of the app.
-       *
-       * See `app.kubernetes.io/version` from:
-       * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-       */
-      'app.kubernetes.io/version'?: string;
-    };
-    /**
-     * The name of the app.
-     *
-     * See `app.kubernetes.io/name` from:
-     * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-     */
-    name: string;
-    /**
-     * The namespace of the app.
-     */
-    namespace: string;
-  };
-  spec: {
-    ports?: {
-      name?: string;
-      port: number;
-      protocol: string;
-    }[];
-  };
-};
 export default {
+  components: {
+    Card,
+    ButtonDropDown,
+  },
+  computed: {
+    endpoints() {
+      return (
+        this.service?.spec.ports?.map((port) => {
+          const endpoint = `${
+            isMaybeSecure(port.port, port.protocol) ? 'https' : 'http'
+          }:${this.service.metadata.name}:${port.port}`;
+
+          return {
+            label: `${endpoint}${port.protocol === 'UDP' ? ' (UDP)' : ''}`,
+            value: `k8s/clusters/${this.clusterId}/api/v1/namespaces/${this.service.metadata.namespace}/services/${endpoint}/proxy`,
+          };
+        }) ?? []
+      );
+    },
+    computedServiceName() {
+      return (
+        this.service?.metadata.labels?.['app.kubernetes.io/component'] ??
+        this.service?.metadata.name
+      );
+    },
+    helmChart() {
+      return this.service?.metadata.labels?.['helm.sh/chart'];
+    },
+    kubernetesVersion() {
+      return this.service?.metadata.labels?.['app.kubernetes.io/version'];
+    },
+    name() {
+      return this.service?.metadata.name;
+    },
+    namespace() {
+      return this.service?.metadata.namespace;
+    },
+  },
+  props: {
+    clusterId: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: Object,
+      required: true,
+      default: () => ({
+        id: '',
+        metadata: {
+          labels: {
+            /**
+             * Helm chart name that created this app (if relevant).
+             */
+            'helm.sh/chart': '',
+            /**
+             * The component of the app.
+             *
+             * See `app.kubernetes.io/component` from:
+             * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
+             */
+            'app.kubernetes.io/component': '',
+            /**
+             * The version of the app.
+             *
+             * See `app.kubernetes.io/version` from:
+             * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
+             */
+            'app.kubernetes.io/version': '',
+          },
+          /**
+           * The name of the app.
+           *
+           * See `app.kubernetes.io/name` from:
+           * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
+           */
+          name: '',
+          /**
+           * The namespace of the app.
+           */
+          namespace: '',
+        },
+        spec: {
+          ports: [],
+        },
+      }),
+    },
+  },
+  methods: {
+    openLink(link) {
+      window.open(link);
+    },
+  },
+  name: 'AppLauncherCard',
   layout: 'plain',
 };
 </script>
