efbf0a0ebf5dbadc152aea5ce277cbe3c42e1350
John Gardner <john@ardea.io>
2024-03-12 14:04:35 -0400

    feat: for APPLAUNCHER-27 combined app service cards with ingress cards

--- pkg/app-launcher/components/AppLauncherCard.vue
+++ pkg/app-launcher/components/AppLauncherCard.vue
@@ -2,6 +2,7 @@
 import { Card } from '@components/Card';
 import ButtonDropDown from '@shell/components/ButtonDropdown';
 import { isMaybeSecure } from '@shell/utils/url';
+import { ingressFullPath } from '@shell/models/networking.k8s.io.ingress';
 
 export default {
   components: {
@@ -19,46 +20,11 @@ export default {
     },
     service: {
       type: Object,
-      required: true,
-      default: () => ({
-        id: '',
-        metadata: {
-          labels: {
-            /**
-             * Helm chart name that created this app (if relevant).
-             */
-            'helm.sh/chart': '',
-            /**
-             * The component of the app.
-             *
-             * See `app.kubernetes.io/component` from:
-             * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-             */
-            'app.kubernetes.io/component': '',
-            /**
-             * The version of the app.
-             *
-             * See `app.kubernetes.io/version` from:
-             * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-             */
-            'app.kubernetes.io/version': '',
-          },
-          /**
-           * The name of the app.
-           *
-           * See `app.kubernetes.io/name` from:
-           * https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/#labels
-           */
-          name: '',
-          /**
-           * The namespace of the app.
-           */
-          namespace: '',
-        },
-        spec: {
-          ports: [],
-        },
-      }),
+      default: null,
+    },
+    ingress: {
+      type: Object,
+      default: null,
     },
   },
   methods: {
@@ -70,12 +36,15 @@ export default {
     },
   },
   computed: {
+    app() {
+      return this.service || this.ingress;
+    },
     endpoints() {
       return (
-        this.service?.spec.ports?.map((port) => {
+        this.service?.spec?.ports?.map((port) => {
           const endpoint = `${
             isMaybeSecure(port.port, port.protocol) ? 'https' : 'http'
-          }:${this.service.metadata.name}:${port.port}`;
+          }:${this.service?.metadata?.name}:${port.port}`;
 
           return {
             label: `${endpoint}${port.protocol === 'UDP' ? ' (UDP)' : ''}`,
@@ -86,32 +55,35 @@ export default {
     },
     computedServiceName() {
       return (
-        this.service?.metadata.labels?.['app.kubernetes.io/component'] ??
-        this.service?.metadata.name
+        this.service?.metadata?.labels?.['app.kubernetes.io/component'] ??
+        this.service?.metadata?.name
       );
     },
     helmChart() {
-      return this.service?.metadata.labels?.['helm.sh/chart'];
+      return this.service?.metadata?.labels?.['helm.sh/chart'];
     },
     kubernetesVersion() {
-      return this.service?.metadata.labels?.['app.kubernetes.io/version'];
+      return this.service?.metadata?.labels?.['app.kubernetes.io/version'];
     },
     name() {
-      return this.service?.metadata.name;
+      return this.service?.metadata?.name;
     },
     namespace() {
-      return this.service?.metadata.namespace;
+      return this.service?.metadata?.namespace;
     },
     isFavorited() {
       return this.favoritedServices.some(
         (favoritedService) =>
-          favoritedService.clusterId === this.clusterId &&
-          favoritedService.service.id === this.service.id
+          favoritedService?.clusterId === this.clusterId &&
+          favoritedService?.service?.id === this.service?.id
       );
     },
     isGlobalApp() {
-      return this.service.metadata.annotations?.['extensions.applauncher/global-app'] === 'true';
+      return this.service?.metadata?.annotations?.['extensions.applauncher/global-app'] === 'true';
     },
+    ingressPath() {
+      return ingressFullPath(this.ingress, this.ingress?.spec?.rules?.[0]);
+    }
   },
   name: 'AppLauncherCard',
   layout: 'plain',
@@ -123,27 +95,24 @@ export default {
     <template #title>
       <div style="width: 100%">
         <p style="font-size: 1.2rem">
-          {{ computedServiceName }}
+          {{ service ? service?.metadata?.name : ingress?.metadata?.name }}
         </p>
-        <div
-          style="
-            color: var(--input-label);
-            display: flex;
-            justify-content: space-between;
-            margin-top: 4px;
-          "
-        >
-          <p v-if="kubernetesVersion !== undefined">
+        <div style="color: var(--input-label); display: flex; justify-content: space-between; margin-top: 4px;">
+          <p v-if="app.type === 'service' && app.metadata?.labels?.['app.kubernetes.io/version'] !== undefined">
             {{ kubernetesVersion }}
           </p>
-          <p v-if="helmChart !== undefined">
+          <p v-if="app.type === 'service' && app.metadata?.labels?.['helm.sh/chart'] !== undefined">
             {{ helmChart }}
+          </p>
+          <p v-if="app.type === 'ingress'">
+            Ingress
           </p>
         </div>
       </div>
     </template>
     <template #body>
-      <p>{{ namespace }}/{{ name }}</p>
+      <p v-if="app.type === 'service'">{{ namespace }}/{{ name }}</p>
+      <p v-if="app.type === 'ingress'">{{ ingressPath }}</p>
     </template>
     <template #actions>
       <button v-if="!isGlobalApp" class="icon-button" @click="toggleFavorite">
@@ -151,7 +120,7 @@ export default {
       </button>
       <i v-else class="icon icon-globe icon-only" />
       <a
-        v-if="(endpoints?.length ?? 0) <= 1"
+        v-if="(endpoints?.length ?? 0) <= 1 && app.type === 'service'"
         :disabled="!endpoints?.length"
         :href="endpoints[0]?.value"
         target="_blank"
@@ -159,6 +128,15 @@ export default {
         :title="endpoints?.length === 0 ? t('appLauncher.noEndpointFoundForApp') : t('appLauncher.launchEndpoint', {
           endpoint: endpoints[0].label,
         })"
+        class="btn role-primary"
+      >
+        {{ t('appLauncher.launch') }}
+      </a>
+      <a
+        v-else-if="app.type === 'ingress'"
+        :href="ingressPath"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
         class="btn role-primary"
       >
         {{ t('appLauncher.launch') }}
